efc56ba0f7238832510d0531b7d60b5d444ff969
Sergeynsk63 <sergey.nsk63@gmail.com>
2026-03-04 19:17:10 +0400

    Initial Commit

---
+++ pomodoroTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pomodoroTimer
+//
+//  Created by Sergey on 04.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pomodoroTimer/pomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoroTimerApp.swift
+//  pomodoroTimer
+//
+//  Created by Sergey on 04.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct pomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
